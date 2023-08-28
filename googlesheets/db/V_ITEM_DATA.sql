a828c63adbcd3ad0c9df8f69fb0e53add5bba00b
k4r3l1nS <makcum.kyp4ehkob@gmail.com>
2023-08-28 16:18:43 +0300

    Performed new clothing item DB view sql code

--- googlesheets/db/V_ITEM_DATA.sql
+++ googlesheets/db/V_ITEM_DATA.sql
@@ -5,7 +5,9 @@ SELECT
     I.clothing_type,
     I.name,
     I.selling_price,
-    ic.storage_url as icon
+    ic.storage_url as main_icon
 FROM Item I
-LEFT JOIN icon ic on ic.id = I.icon_id
+LEFT JOIN item_icons ii on I.id = ii.items_id
+LEFT JOIN icon ic on ii.icons_id = ic.id
 WHERE I.is_active = true
+AND (ic.is_main = true OR ic.storage_url is NULL)
